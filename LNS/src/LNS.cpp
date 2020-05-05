29432a2afbc936ba3c8c483ed5c95e8338a7a50e
Madhava <madhavavsih@gmail.com>
2020-05-06 01:23:00 +0530

    Fixed cmake not finding spdlog directory, added quick test to see if spdlog works

--- LNS/src/LNS.cpp
+++ LNS/src/LNS.cpp
@@ -1,9 +1,11 @@
 #include <LNS.h>
+#include <spdlog/spdlog.h>
 
 namespace LNS
 {
     std::string TestFunction()
     {
+        spdlog::error("Some error message with arg: {}", 1);
         return std::string("First Commit Testing 1...2...3...!");
     }
 }
