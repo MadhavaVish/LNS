c1520d31f7ca9ee1e20d88a8fa8e8ad4c4092315
maddy <madhavavsih@gmail.com>
2020-05-06 18:00:51 +0530

    Made CmakeLists work on Linux, added cross platform shared lib exports to test

--- LNS/include/LNS.h
+++ LNS/include/LNS.h
@@ -1,8 +1,15 @@
 #pragma once
+#pragma once
+
+#ifdef LNS_PLATFORM_WINDOWS
+    #define LNS_API __declspec(dllexport)
+#else
+	#define LNS_API 
+#endif
 
 #include <string>
 
 namespace LNS
 {
-    __declspec(dllexport) std::string TestFunction();
+    LNS_API std::string TestFunction();
 }
